12d7c86983bda9fba311dbc76087ea6de90880eb
yura <wa7er14@gmail.com>
2020-06-21 23:41:04 +0300

    Updates

---
+++ precache-manifest.6f923d94f8100c7d7cf264a1ecec30fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f65ccf8b54d8486b1678f70c56e0123c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "43017edd4762513ac25a",
+    "url": "./static/css/main.1e12a234.chunk.css"
+  },
+  {
+    "revision": "b0dff7fd6bb18487afb8",
+    "url": "./static/js/2.0ce2c40f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.0ce2c40f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43017edd4762513ac25a",
+    "url": "./static/js/main.b092e10e.chunk.js"
+  },
+  {
+    "revision": "8b3c3b96386eee1ea33a",
+    "url": "./static/js/runtime-main.b672bc02.js"
+  }
+]);
